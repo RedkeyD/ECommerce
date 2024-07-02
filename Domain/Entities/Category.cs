58629cfc0972204f45f554a2eada7d2c2d2c352c
RedKeyD <redkeydead@gmail.com>
2024-07-02 11:19:35 +0300

    поправил домейн слой

--- Domain/Entities/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entities;
-
-public class Category
-{
-    public Guid Id { get; }
-    public string Name { get; }
-    public string Description { get; }
-    public ICollection<Product> Products { get; }
-}
